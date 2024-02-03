59ef945e1fe328e3b53333ac3d2bf2c2826c8fb0
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-03 15:20:20 +0530

    Created user routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successful"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(process.env.port || 5000, () => {
   console.log("Server running on 5000");
