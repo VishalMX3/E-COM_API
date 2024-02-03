cf2599474985a17687e4e03b6b743ab93b3149d1
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-03 20:19:59 +0530

    Successfuly created register and login routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 
+//REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     userName: req.body.userName,
@@ -18,6 +19,27 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
     console.log(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ userName: req.body.userName });
+    !user && res.status(401).json("Wrong U-Credentials");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    password !== req.body.password &&
+      res.status(401).json("Wrong P-Credentials");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
