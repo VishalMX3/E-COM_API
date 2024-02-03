a20663b6a932d4a92fcd926d8c0ebb5c1a8ec76d
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-03 19:23:47 +0530

    New user created successfully

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
     userName: req.body.userName,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
-    console.log(savedUser);
+    res.status(201).json(savedUser);
   } catch (err) {
+    res.status(500).json(err);
     console.log(err);
   }
 });
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,3 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("User Test is successful!");
-});
-
-router.post("/userposttest", (req, res) => {
-  const userName = req.body.userName;
-  res.send("Your user name is: " + userName);
-});
-
 module.exports = router;
