37b4d1ac1d92151fec2e4886608f86916948b964
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-05 11:22:15 +0530

    get all users info

--- routes/user.js
+++ routes/user.js
@@ -39,13 +39,28 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-//GET
+//GET user
 
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
     res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get all users
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(1)
+      : await User.find();
+
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
   }
