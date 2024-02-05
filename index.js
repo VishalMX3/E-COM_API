374f07ce34d10cbe77e3e0eaefeed4ff69d97a08
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-05 17:44:22 +0530

    Orders CRUD completed

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.port || 5000, () => {
   console.log("Server running on 5000");
